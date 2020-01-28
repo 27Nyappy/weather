a5af1a3a48b524a18ae64e4f601933a536e26420
antonella <antonella.gutierrezrodriguez@pcc.edu>
2020-01-27 22:52:13 -0800

    off to writing component that renders weather

--- src/containers/Weather.js
+++ src/containers/Weather.js
@@ -1,14 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import WeatherForm from '../components/WeatherForm';
 import getWeather from '../services/WeatherApi';
 
 const Weather = () => {
   const [zip, updateZip] = useState('');
   const [days, updateDays] = useState('');
+  const [weatherInfo, updateWeatherInfo] = useState([]);
 
   const handleSubmit = event => {
     event.preventDefault();
-    getWeather(days, zip);
+    getWeather(days, zip)
+      .then(res => updateWeatherInfo(res));
   };
 
   const handleDayChange = ({ target }) => updateDays(target.value);
--- src/services/WeatherApi.js
+++ src/services/WeatherApi.js
@@ -3,12 +3,12 @@ import axios from 'axios';
 const key = process.env.WEATHER_KEY;
 
 export default function getWeather(time, code) {
+
   switch(time) {
     case 'weather':
-      axios.get(`http://api.openweathermap.org/data/2.5/${time}?zip=${code},us&units=imperial&appid=${key}`)
+      return axios.get(`http://api.openweathermap.org/data/2.5/${time}?zip=${code},us&units=imperial&appid=${key}`)
         .then(res => {
           const date = Date(res.data.dt);
-          console.log(res.data);
           const weatherObjDay = {
             main: res.data.main.temp,
             feelsLike: res.data.main.feels_like,
@@ -18,21 +18,19 @@ export default function getWeather(time, code) {
             detail: res.data.weather[0].main,
             description: res.data.weather[0].description,
             icon: `http://openweathermap.org/img/wn/${res.data.weather[0].icon}@2x.png`,
-            detailTwo: res.data.weather[1].main,
-            descriptionTwo: res.data.weather[1].description,
-            iconTwo: `http://openweathermap.org/img/wn/${res.data.weather[1].icon}@2x.png`,
             date: date,
             city: res.data.name
           };
           return weatherObjDay;
         })
-        .catch(err => console.log(err));
-      break;
+        .catch(err => {
+          throw err;
+        });
     case 'forecast':
-      axios.get(`http://api.openweathermap.org/data/2.5/${time}?zip=${code},us&units=imperial&appid=${key}`)
+      return axios.get(`http://api.openweathermap.org/data/2.5/${time}?zip=${code},us&units=imperial&appid=${key}`)
         .then(res => {
-          res.data.list.map(({ main, weather, dt }) => { 
-            const dates = Date(dt);
+          return res.data.list.map(({ main, weather, dt_txt }) => {
+            const dates = new Date(`${dt_txt}`).toString();
             const weatherObjDays = {
               main: main.temp,
               feelsLike: main.feels_like,
@@ -48,7 +46,8 @@ export default function getWeather(time, code) {
             return weatherObjDays;
           }
           );})
-        .catch(err => err);
-      break;
+        .catch(err => {
+          throw err;
+        });
   }
 }
