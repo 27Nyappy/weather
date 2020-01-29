8f97465a71f6757605e7b9bd100db5c984dd5941
antonella <antonella.gutierrezrodriguez@pcc.edu>
2020-01-28 22:42:49 -0800

    minor css change

--- src/components/WeatherForm.js
+++ src/components/WeatherForm.js
@@ -5,15 +5,21 @@ import styles from './WeatherForm.css';
 const WeatherForm = ({ handleSubmit, handleZipChange, handleDayChange }) => {
   return (
     <form onSubmit={handleSubmit} className={styles.WeatherForm}>
-      <h2>Please Enter Your Zip Code</h2>
-      <input type="number" max="99950" min="00501" onChange={handleZipChange} placeholder="ZIP" required />
-      <h2>Select Time</h2>
-      <div>
-        <input type="radio" name="day-options" id="current-weather" onChange={handleDayChange} value="weather" required />
-        <label htmlFor="current-weather">Current Weather</label>
-        <input type="radio" name="day-options" id="five-day-weather" onChange={handleDayChange} value="forecast" required />
-        <label htmlFor="five-day-weather">Five Day Forecast</label>
-      </div>
+      <section>
+        <div id="zip">
+          <h2>Please Enter Your Zip Code</h2>
+          <input type="number" max="99950" min="00501" onChange={handleZipChange} placeholder="ZIP" required />
+        </div>
+        <div id="days">
+          <h2>Select Time</h2>
+          <div id="selections">
+            <input type="radio" name="day-options" id="current-weather" onChange={handleDayChange} value="weather" required />
+            <label htmlFor="current-weather">Current Weather</label>
+            <input type="radio" name="day-options" id="five-day-weather" onChange={handleDayChange} value="forecast" required />
+            <label htmlFor="five-day-weather">Five Day Forecast</label>
+          </div>
+        </div>
+      </section>
       <input type="submit" value="Get Weather" />
     </form>
   );
